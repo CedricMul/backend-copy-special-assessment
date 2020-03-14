355bd1d6579354b3a72fc0810bdecf914aa013da
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-03-13 21:10:01 -0400

    completed, let me know if I need some error except

--- copyspecial.py
+++ copyspecial.py
@@ -11,24 +11,82 @@
 
 import re
 import os
+import os.path
 import shutil
 import subprocess
 import argparse
+import sys
 
 # This is to help coaches and graders identify student assignments
-__author__ = "???"
+__author__ = "Cedric Mulvihill"
 
 
 # +++your code here+++
 # Write functions and modify main() to call them
+
+def get_special_paths(dir):
+    dirList = os.listdir(dir)
+    specialList = []
+    literalSpecial = re.compile(r'__[a-zA-Z0-9]+[-_]*[a-zA-Z0-9]*__')
+    for i in dirList:
+        if literalSpecial.search(i):
+            p = os.path.abspath(i)
+            specialList.append(p)
+    return specialList
+
+
+def copy_to(paths, dir):
+    if '/' in dir:
+        dirList = dir.split('/')
+        dirList.pop()
+        if len(dirList) > 1:
+            dirPath = dirList.join('/')
+        else:
+            dirPath = dirList[0]
+    else:
+        dirPath = dir
+    
+    if not os.path.isdir(dirPath):
+        os.makedirs(dirPath)
+    for a in paths:
+        shutil.copy(a, dir)
+
+def zip_to(paths, zippath):
+    cString = 'zip -j zipfile'
+    for p in paths:
+        cString += ' ' + p
+    print("Executing command: " + cString)
+    os.system(cString)
+
 
 def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO need an argument to pick up 'from_dir'
+    parser.add_argument('--from_dir', help='return list of special files from dir')
+    parser.add_argument('dir', help='a single directory search', nargs='?')
     args = parser.parse_args()
+
+    if not args:
+        parser.print_usage()
+        sys.exit()
+
+    if not args.from_dir:
+        x = get_special_paths(args.dir)
+        for i in x:
+            print(i)
+    else:
+        fromPaths = get_special_paths(args.from_dir)
+        if args.todir:
+            copy_to(fromPaths, args.todir)
+        elif args.tozip:
+            zip_to(fromPaths, args.tozip)
+        else:
+            parser.print_usage()
+
+
+
 
     # TODO you must write your own code to get the cmdline args.
     # Read the docs and examples for the argparse module about how to do this.
